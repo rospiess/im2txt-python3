c20c20de2c547e2e034057551e4ba44d9f18cade
Yanping <rea.s.xyp@gmail.com>
2018-01-03 20:47:46 +0100

    file path

--- im2txt/evaluation/evaluate_im2txt.py
+++ im2txt/evaluation/evaluate_im2txt.py
@@ -6,7 +6,7 @@ dataDir='../data/mscoco/raw-data/'
 dataType='val2014'
 algName = 'im2txt'
 annFile='%s/annotations/captions_%s.json'%(dataDir,dataType)
-resFile= './results/captions_%s_%s_results.json'%(dataType, algName)
+resFile= 'captions_%s_%s_results.json'%(dataType, algName)
 
 # create coco object and cocoRes object
 coco = COCO(annFile)
