c4041fc4bef881b95db4a49cdf18614956efea02
Yanping <rea.s.xyp@gmail.com>
2018-01-10 20:51:31 +0100

    not calculate score here

--- im2txt/evaluate_metrics.py
+++ im2txt/evaluate_metrics.py
@@ -1,8 +1,5 @@
-
 import math
-import numpy as np
 import os
-import nltk
 import json
 
 import tensorflow as tf
@@ -92,61 +89,33 @@ def main(_):
     # available beam search parameters.
     generator = caption_generator.CaptionGenerator(model, vocab)
 
-    global_bleu1_scores = []
-    global_bleu2_scores = []
-    global_bleu4_scores = []
-
     output_captions = []
 
     for t, img_id in enumerate(test_ids):
         filename = id_to_filename[img_id]
-        try:
-          with tf.gfile.GFile(filename, "rb") as f:
+        with tf.gfile.GFile(filename, "rb") as f:
             image = f.read()
-        except Exception:
-          print("skip id",img_id,filename)
-          continue
-        #captions = generator.beam_search(sess, image)
-        captions = generator.bulb_beam_search(sess, image)
+
+        captions = generator.beam_search(sess, image)
+
+        first_sentence = " ".join([vocab.id_to_word(w) for w in captions[0].sentence[1:-1]])
+        output_captions.append({"image_id": img_id, "caption": first_sentence})
+
         if FLAGS.verbose:
             print("Captions for image %s:" % os.path.basename(filename))
-        bleu1_scores = []
-        bleu2_scores = []
-        bleu4_scores = []
-        first_sentence = " ".join([vocab.id_to_word(w) for w in captions[0].sentence[1:-1]])
-        print(first_sentence)
-        output_captions.append({"image_id": img_id, "caption": first_sentence})
-        for i, caption in enumerate(captions):
-            # Ignore begin and end words.
-            sentence = [vocab.id_to_word(w) for w in caption.sentence[1:-1]]
-            bleu1_scores.append(nltk.translate.bleu(id_to_captions[img_id], sentence, weights=[1.]))
-            bleu2_scores.append(nltk.translate.bleu(id_to_captions[img_id], sentence, weights=[0.5, 0.5]))
-            bleu4_scores.append(nltk.translate.bleu(id_to_captions[img_id], sentence, weights=[0.25, 0.25, 0.25, 0.25]))
-            if FLAGS.verbose:
+            for i, caption in enumerate(captions):
+                # Ignore begin and end words.
+                sentence = [vocab.id_to_word(w) for w in caption.sentence[1:-1]]
                 sentence = " ".join(sentence)
                 print("  %d) %s (p=%f)" % (i, sentence, math.exp(caption.logprob)))
-                print("Bleu:", bleu4_scores[-1])
-        global_bleu1_scores.append(max(bleu1_scores))
-        global_bleu2_scores.append(max(bleu2_scores))
-        global_bleu4_scores.append(max(bleu4_scores))
+
         if FLAGS.verbose:
             for caption in id_to_captions[img_id]:
                 print(caption)
-            print(np.mean(bleu1_scores))
-            print(np.mean(bleu2_scores))
-            print(np.mean(bleu4_scores))
+
         if t % 10 == 0:
             print("%d / %d" % (t,len(test_ids)))
-            print(np.mean(global_bleu1_scores))
-            print(np.mean(global_bleu2_scores))
-            print(np.mean(global_bleu4_scores))
-        if t > 100:
-            break
 
-    print("Mean Bleu-1, Bleu-2, Bleu-4 scores:")
-    print(np.mean(global_bleu1_scores))
-    print(np.mean(global_bleu2_scores))
-    print(np.mean(global_bleu4_scores))
 
     with open('captions_val2014_im2txt_results.json', 'w') as outfile:
         json.dump(output_captions, outfile)
